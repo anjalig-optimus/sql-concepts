676fd5924104379a385c549a864f7414a5bf4d80
anjalig-optimus <anjali.gupta@optimusinfo.com>
2024-10-07 18:17:39 +0530

    dml commands

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,3 +11,38 @@ create table student1
  Insert into student1(ID,FirstName)
  Values(2,'xyz')
  select * from student1;
+ 
+ -- update
+ update student1
+ set ID=10 
+ where FirstName='xyz'
+ 
+ Insert into student1(ID,FirstName)
+ Values(100,'abc')
+ select * from student1
+
+ --delete
+ delete from student1 where id=10
+
+ -- add column
+ alter table student1
+ add phoneno INT
+ 
+ update student1
+ set phoneno=9058
+ where id=100
+ update student1
+ set phoneno=9315
+ where id=1
+ select* from student1
+ 
+ -- aggregate functions
+ -- count
+ select count(*) from student1
+
+ --min & max
+ select min(id) from student1
+ select max(id) from student1
+
+ -- sum & avg
+ select sum(phoneno) from student1
