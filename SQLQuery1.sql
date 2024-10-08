b541b6ab739f3827bf2f1aa63b89601738a8551b
anjalig-optimus <anjali.gupta@optimusinfo.com>
2024-10-08 18:12:49 +0530

    all commands under sql tutorial and sql db sections

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -46,3 +46,91 @@ create table student1
 
  -- sum & avg
  select sum(phoneno) from student1
+
+create database teacher;
+go
+drop database teacher
+
+create table prof
+(
+ teacherid INT Primary key,
+ email varchar(50),
+ teachername varchar(50)
+ );
+ select * from prof
+ Insert into prof values(1,'anjali@gmail.com','anjali'),(2,'naveen@gmail.com','naveen'),(3,'kishan@gmail.com','kishan')
+ 
+ -- wildcard
+ select * from prof
+ where teachername like 'a%';
+
+ alter table prof
+ add age INT
+ update prof
+ set age=21
+ where teacherid=1
+ update prof
+ set age=20
+ where teacherid=2
+ update prof
+ set age=22
+ where teacherid=3
+
+ --operators
+ select teachername from prof where age in(22,23)
+ select teachername from prof where age between 22 and 25
+
+ --aliases
+ select teachername as intern from prof
+ select * from prof as optimus
+ select teacherid as id from prof
+
+ --joins
+ select student1.FirstName ,prof.teachername from student1 
+ inner join prof on prof.teacherid =student1.id 
+
+ select * from prof
+ left join student1
+ on student1.id=prof.teacherid
+
+ select * from prof
+ right join student1
+ on student1.id=prof.teacherid
+
+  select * from prof
+ full outer join student1
+ on student1.id=prof.teacherid
+
+ --union
+ select id from student1
+ union
+ select teacherid from prof
+
+ Insert into prof values (4,'deepak@gmail.com','deepak',22)
+
+ --groupby
+select count(age) as ageCount, teachername from prof
+group by teachername
+
+SELECT count(age) as Counting,age FROM prof group by age;
+
+--having
+select count(age) from prof group by age having age<22
+
+--all
+select all age
+from prof where age>20
+
+Insert into prof values (5,'harsh@google.com','harsh',null)
+
+--cases
+select teachername ,teacherid,age from prof
+order by (
+case
+   when age is null then teacherid
+   else age
+end);
+
+select teachername ,teacherid,age from prof
+order by age desc;
+
