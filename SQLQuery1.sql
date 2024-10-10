0774a5891d29f71b477080353740e4a8b3428ddf
anjalig-optimus <anjali.gupta@optimusinfo.com>
2024-10-10 18:42:04 +0530

    advanced sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -134,3 +134,103 @@ end);
 select teachername ,teacherid,age from prof
 order by age desc;
 
+ --views
+ create table employee2
+ (
+ id int primary key,
+ first_name varchar(50),
+ last_name varchar(50),
+ salary int,
+ joining_date date
+ );
+
+ create view hire_employee
+ as
+ select e.id,e.first_name,e.last_name
+ from employee2 as e
+
+ insert into employee2 values (1,'Anjali','Gupta',5000,'2024-10-03'),(2,'Deepak','Gupta',500,'2024-10-04'),(3,'Kishan','Payadi',900,'2024-10-03')
+ select * from hire_employee
+
+ create table block
+ (
+ block_id int Primary key,
+ block_name varchar(50),
+ salary int 
+ );
+
+ insert into block values (101,'kalpana',5000),(102,'bhabha',800),(103,'aryabhatt',900)
+
+ create view employee_block as
+ select e.id,e.first_name from employee2 as e join block as b on e.salary=b.salary
+ where id>0
+
+  create view employee_block2 as
+ select e.id,e.first_name, b.block_name from employee2 as e join block as b on e.salary=b.salary
+ where id>0
+
+ select * from employee_block2
+
+ insert into employee_block values (5,'vaibhav') 
+  insert into employee_block values(6,'naveen')
+
+  -- we can't insert new rows in the view which is created by the join of two tables
+insert into employee_block2 values(7,'naveen', 'Kalpana')
+
+--cte
+--help to inc readiness and more efficient
+-- to make queries less complex
+--mostly used in replace of sql
+
+--cte query
+WITH AverageSalary AS (
+    SELECT AVG(Salary) AS AvgSalary
+    FROM employee2
+)
+SELECT e.first_name, e.Salary, a.AvgSalary
+FROM employee2 e, AverageSalary a
+WHERE e.Salary > a.AvgSalary;
+
+--stored procedure
+--reduce network traffic
+--db becomes more secure
+--can be used over and over again
+--stored in cache
+
+create procedure sp
+as
+begin
+select * from employee2 where id=1
+end
+
+create procedure sp2
+as
+begin
+select * from employee2 where id=1
+select * from employee2 where first_name='kishan'
+end
+
+--calling a procedure just by writing its name or execute name
+exec sp
+sp2
+
+--drop proc
+drop proc sp2
+
+--alter proc with parameters
+alter proc sp
+@emp2_name varchar(50)
+as
+begin
+select * from employee2 where first_name=@emp2_name;
+end
+
+sp 'kishan'
+
+--TRIGGERS
+--ENFORCE DATA INTEGRITY
+--effective when dml commands take place
+--AFTER/FOR TRIGGERS OCCUR AFTER THE DML COMMANDS
+--INSTEAD TRIGGER USED FOR OVERRIDING
+
+SELECT CAST(25.4 AS INT)
